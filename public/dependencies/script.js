41d5bd8ccc19624ca2ba0959522cdedda577e45e
avophile <douglas.locklin@gmail.com>
2019-05-18 04:24:52 +0300

    displays opponents choice

--- public/dependencies/script.js
+++ public/dependencies/script.js
@@ -60,30 +60,50 @@ function playPaper(num){
 //Compare player1, player2 or machine answers together
 function compare(choice1, choice2) {
     if (choice2 == "Spock"){
-        document.getElementById("countdown").innerHTML = "Spock refutes you loser!";
+        document.getElementById("countdown").innerHTML = "Spock refutes you, loser!";
         console.log("Player 1 Loses! SPock always wins!");
     } else {
             console.log(choice1 + choice2);
+            switch(choice2) {
+                case "r":
+                document.getElementById("start_button").src = "dependencies/images/rock_opponent.png";
+                break;
+                case "s":
+                document.getElementById("start_button").src = "dependencies/images/scissors_opponent.png";
+                break;
+                case "p":
+                document.getElementById("start_button").src = "dependencies/images/paper_opponent.png";
+                break;
+            }
             switch(choice1 + choice2) {
                 case "rs":
                 case "pr":
                 case "sp":
                 document.getElementById("countdown").innerHTML = "You win!";
-                
+                setTimeout(function(){ 
+                    document.getElementById("countdown").innerHTML = "Ready?"; 
+                    document.getElementById("start_button").src = "dependencies/images/start_button.jpg";
+                    }, 3000);
                 console.log("Player 1 Wins!");
-                breasetTimeout(function(){ document.getElementById("countdown").innerHTML = "Ready?"; }, 3000);k;
+                break;
                 case "rp":
                 case "ps":
                 case "sr":
                 document.getElementById("countdown").innerHTML = "You lose!";
-                setTimeout(function(){ document.getElementById("countdown").innerHTML = "Ready?"; }, 3000);
+                setTimeout(function(){ 
+                    document.getElementById("countdown").innerHTML = "Ready?"; 
+                    document.getElementById("start_button").src = "dependencies/images/start_button.jpg";
+                    }, 3000);
                 console.log("Player 2 Wins!");
                 break;
                 case "rr":
                 case "pp":
                 case "ss":
                 document.getElementById("countdown").innerHTML = "It's a tie!";
-                setTimeout(function(){ document.getElementById("countdown").innerHTML = "Ready?"; }, 3000);
+                setTimeout(function(){ 
+                    document.getElementById("countdown").innerHTML = "Ready?";
+                    document.getElementById("start_button").src = "dependencies/images/start_button.jpg";
+                    }, 3000);
                 console.log("Draw!");
                 break;
             }
