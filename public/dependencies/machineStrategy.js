38b0aba4e2d040cec44f17bda25938bb57d9079a
avophile <douglas.locklin@gmail.com>
2019-05-20 01:47:42 +0300

    makes game logic more modular

--- public/dependencies/machineStrategy.js
+++ public/dependencies/machineStrategy.js
@@ -1,120 +1,125 @@
+// player chooses machine strategy
 function selectMachineStrategy(num){
-    switch (num) {
-    case '1':
-        machineChoice = getRock();
-        break;
-    case '2':
-        machineChoice = getScissors();
-        break;
-    case '3':
-        machineChoice = getPaper();
-        break; 
-    case '4':
-        machineChoice = getMachineRandomChoice();
-        break;
-    case '5':
-        machineChoice = trackAndCounterOpponentPreviousChoice(choice1);
-        break;
-    case '6':
-        machineChoice = decayPreviousChoiceValues(choice1);
-        break;
-    case '7':
-        machineChoice = getSpock();
-        break;
-    case '8':
-        machineChoice = getFU();
-        break;
-    default: 
-        machineChoice = getMachineRandomChoice();
-    }
-    console.log("The player selected strategy number " + num + ", " + machineChoice + " for the machine.");
+    machineStrategySelection=num;
+    console.log("The player selected strategy number " + machineStrategySelection + " for the machine.");
+    return machineStrategySelection;
 }
 
-// Function computer plays spock and wins automatically
-function getSpock(){
-    var choices = ["Spock!"];
-    return choices;
+function getMachineChoice(machineStrategySelection){
+    machineStrategyFunctionNumber=machineStrategySelection;
+    console.log(machineStrategyFunctionNumber);
+    machineChoice = machineStrategyFunctions.machineStrategyFunctionNumber;
+    console.log("machineChoice is " + machineChoice)
+    compare(playerOneChoice, machineChoice);
+}   
+
+/*function executeMachineStrategy(machineStrategySelection){
+    machineStrategyFunctionNumber=machineStrategySelection;
+    machineStrategyFunctions.machineStrategyFunctionNumber;
+    console.log("The machine called strategy function number " + machineStrategyFunctionNumber + ", " + machineStrategyFunctions[machineStrategyFunctionNumber] + ".");
+    return ;
+}*/
+
+//Function to randomise computer choice
+function getRandomMachineStrategy() {
+    selectMachineStrategy(Math.floor(Math.random()*8));
 }
 
-// Function computer chooses rock
-function getRock() {
-    var choices = ["r", "s", "p"];
-    return choices[0];
-}
+var machineStrategyFunctions = { // array stores all the machines strategies
 
-// Function computer chooses scissors
-function getScissors() {
-    var choices = ["r", "s", "p"];
-    return choices[1];
-}
+    // Function 0: computer plays Spock and wins automatically
+    0: function getSpock (){
+        return "Spock!";
+    },
 
-// Function computer chooses paper
-function getPaper() {
-    var choices = ["r", "s", "p"];
-    return choices[2];
-}
+    // Function 1: computer chooses rock
+    1: function getRock() {
+        var choices = ["r", "s", "p"];
+        console.log("r");
+        return choices[0];
+    },
 
-// Function to randomize computer choice
-function getMachineRandomChoice() {
-    var choices = ["r", "s", "p"];
-    var rng = Math.floor(Math.random()*3);
-    return choices[rng];
-}
+    // Function 2: computer chooses scissors
+    2: function getScissors() {
+        var choices = ["r", "s", "p"];
+        return choices[1];
+    },
 
-// Function to track and counter opponents previous choices 
-function trackAndCounterOpponentPreviousChoice(choice1){
-    var previousChoices = [rockCount, paperCount, scissorsCount];
-    console.log("The previous choices were " + previousChoices);
+    // Function 3: computer chooses paper
+    3: function getPaper () {
+        var choices = ["r", "s", "p"];
+        return choices[2];
+    },
 
-    if (choice1 === "r"){
-        rockCount += 1;
-    } else if (choice1 === "s"){
-	    paperCount += 1;
-    } else if (choice1 === "p"){
-        scissorsCount += 1;
-    }
+    // Function 4: to randomize computer choice
+    4: function getMachineRandomChoice () {
+        var choices = ["r", "s", "p"];
+        var rng = Math.floor(Math.random()*3);
+        return choices[rng];
+    },
 
-    if ((rockCount > paperCount) && (rockCount > scissorsCount)){ // counters with the choice that beats the choice opponent picks the most frequently
-        return "p"; // paper beats rock
-    } else if (paperCount > scissorsCount){
-        return "s"; // scissors beats paper
-    } else {
-        return "r"; // rock beats scissors
-    }
-}
+    // Function 5: to track and counter opponents previous choices 
+    5: function trackAndCounterOpponentPreviousChoice (choice1){
+        var previousChoices = [rockCount, paperCount, scissorsCount];
+        console.log("The previous choices were " + previousChoices);
 
-//  Function with a decaying score. It is more influenced by recent outcomes than past ones and adapts to a slowly changing opponent. It also reduces the score of the move that would have lost. So, for example, if the opponent played rock, the score for paper would increase, but the score for scissors would decrease.
-function decayPreviousChoiceValues(choice1){
-    if (!choiceRatings){
-        let rockRating = 0;
-        let scissorsRating = 0;
-        let paperRating = 0;
-        let choiceRatings = [rockRating, scissorsRating, paperRating];
-        console.log("The choice ratings are initialized: " + choiceRatings);
-    } else {
-        rockRating *= 0.95;
-        scissorsRating *= 0.95;
-        paperRating *= 0.95;
-        
         if (choice1 === "r"){
-            paperRating += 0.1;
-            scissorsRating -= 0.1;
+            rockCount += 1;
         } else if (choice1 === "s"){
-            rockRating += 0.1;
-            paperRating -= 0.1;
+            paperCount += 1;
         } else if (choice1 === "p"){
-            scissorsRating += 0.1;
-            rockRating -= 0.1;
+            scissorsCount += 1;
         }
-        console.log("The choice ratings are: " + choiceRatings);
-        let randNum = ((random.random())*(math.exp(rockRating))+(math.exp(scissorsRating))+(math.exp(paperRating))); // TODO translate from Python!
 
-        if (randNum < (math.exp(rockRating))){
-            return "r";
-        } else if ((randNum < (math.exp(rockRating))) && (randNum < (math.exp(paperRating)))){
-            return "p";
+        if ((rockCount > paperCount) && (rockCount > scissorsCount)){ // counters with the choice that beats the choice opponent picks the most frequently
+            return "p"; // paper beats rock
+        } else if (paperCount > scissorsCount){
+            return "s"; // scissors beats paper
         } else {
-            return "s";
-        } 
+            return "r"; // rock beats scissors
+        }
+    },
+
+    //  Function 6: with a decaying score. It is more influenced by recent outcomes than past ones and adapts to a slowly changing opponent. It also reduces the score of the move that would have lost. So, for example, if the opponent played rock, the score for paper would increase, but the score for scissors would decrease.
+    6: function decayPreviousChoiceValues (choice1){
+        if (!choiceRatings){
+            let rockRating = 0;
+            let scissorsRating = 0;
+            let paperRating = 0;
+            let choiceRatings = [rockRating, scissorsRating, paperRating];
+            console.log("The choice ratings are initialized: " + choiceRatings);
+        } else {
+            rockRating *= 0.95;
+            scissorsRating *= 0.95;
+            paperRating *= 0.95;
+            
+            if (choice1 === "r"){
+                paperRating += 0.1;
+                scissorsRating -= 0.1;
+            } else if (choice1 === "s"){
+                rockRating += 0.1;
+                paperRating -= 0.1;
+            } else if (choice1 === "p"){
+                scissorsRating += 0.1;
+                rockRating -= 0.1;
+            }
+            console.log("The choice ratings are: " + choiceRatings);
+            let randNum = ((random.random())*(math.exp(rockRating))+(math.exp(scissorsRating))+(math.exp(paperRating))); // TODO translate from Python!
+
+            if (randNum < (math.exp(rockRating))){
+                return "r";
+            } else if ((randNum < (math.exp(rockRating))) && (randNum < (math.exp(paperRating)))){
+                return "p";
+            } else {
+                return "s";
+            } 
+        }
+    },
+
+    // Function 7: computer plays FU and wins automatically
+    7: function getFU (){
+        var choices = ["FU!"];
+        return choices;
     }
-}       
+};
+
